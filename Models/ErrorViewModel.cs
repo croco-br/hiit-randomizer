e280f6bce13c3c4b90f7759da9e7d4ad23a08993
Adriano Croco <adriano.daniel@pravaler.com.br>
2020-07-07 23:04:20 -0300

    chore: add .net core 3.1 dockerfile

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HiitRandomizer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
